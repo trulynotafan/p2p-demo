7702074bf819656ecb945343a048cc986f6fa43b
Afaan <105258264+trulynotafan@users.noreply.github.com>
2025-04-07 13:46:11 +0500

    writer-peer.js

--- writer-peer.js
+++ writer-peer.js
@@ -2,8 +2,8 @@ const b4a = require('b4a')
 const Corestore = require('corestore')
 const Hyperswarm = require('hyperswarm')
 const sodium = require('sodium-universal')
-const derive_seed = require('derive-key')
 const crypto = require("hypercore-crypto")
+const process = require("bare-process") //new module for uptime, though we can use simple counter. But the counter 
 
 start()
 
@@ -12,6 +12,7 @@ start()
 ******************************************************************************/
 async function start (iid) {
   const label = '\x1b[32m[writer]\x1b[0m'
+  const startTime = process.hrtime()
   console.log(label, 'start')
   const opts = {
     namespace: 'noisekeys',
@@ -33,31 +34,43 @@ async function start (iid) {
   const discovery = swarm.join(core.discoveryKey, { server: true, client: false })
   await discovery.flushed()
   return
+
+  function append_more () {
+    const time = Math.floor(process.hrtime(startTime)[0]) //using process.hrtime()
+    const stamp = `${time/60/60|0}h:${time/60|0}m:${time%60}s`
+    core.append(`uptime: ${stamp}`)
+  }
+
   async function onconnection (socket, info) {
     socket.on('error', onerror)
     console.log(label, {conn: 'onconnection', pubkey: info.publicKey.toString('hex')})
     store.replicate(socket)
     iid = setInterval(append_more, 1000)
   }
+
   function onerror (err) {
     clearInterval(iid)
     console.log(label, 'socket error', err)
-  }
-  function append_more () {
-    const time = Math.floor(process.uptime())
-    const stamp = `${time/60/60|0}h:${time/60|0}m:${time%60}s`
-    core.append(`uptime: ${stamp}`)
   }
 }
 /******************************************************************************
   HELPER
 ******************************************************************************/
-function create_noise_keypair ({ namespace, seed, name }) {
-  const noiseSeed = derive_seed(namespace, seed, name)
+function create_noise_keypair ({namespace, seed, name}) {
+  const noiseSeed = deriveSeed(seed, namespace, name)
   const publicKey = b4a.alloc(32)
   const secretKey = b4a.alloc(64)
   if (noiseSeed) sodium.crypto_sign_seed_keypair(publicKey, secretKey, noiseSeed)
   else sodium.crypto_sign_keypair(publicKey, secretKey)
+  
   return { publicKey, secretKey }
 }
 
+function deriveSeed (primaryKey, namespace, name) {
+
+  if (!b4a.isBuffer(namespace)) namespace = b4a.from(namespace) 
+  if (!b4a.isBuffer(name)) name = b4a.from(name)
+  const out = b4a.alloc(32)
+  sodium.crypto_generichash_batch(out, [namespace, name], primaryKey)
+  return out
+}
